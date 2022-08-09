108feb1b42e3f14e95871339217a1e3913fe5a80
jey707 <jey707@naver.com>
2022-08-09 21:21:49 +0900

    tradeApi connect complete

--- js/index.js
+++ js/index.js
@@ -2,6 +2,13 @@ const flight = document.querySelector(".flight");
 const space = document.querySelector(".space");
 const aftermarket = document.querySelector(".aftermarket");
 const busi_img = document.querySelector(".busi_img");
+const btc_price = document.getElementById("btc_price");
+const btc_color = document.getElementById("btc_color");
+const arrow_up = '<i class="fa-solid fa-caret-up"></i>';
+const arrow_down = '<i class="fa-solid fa-caret-down"></i>';
+const eth_price = document.getElementById("eth_price");
+const eth_color = document.getElementById("eth_color");
+// const eth_arrow = document.getElementById("eth_arrow");
 
 // youtube backgroud
 document.addEventListener("DOMContentLoaded", function () {
@@ -46,5 +53,45 @@ function tradePrice(res) {
   console.log(res);
   res.forEach((element) => {
     console.log(element);
+    switch (element.market) {
+      case "KRW-BTC": // 비트시세
+        if (element.change === "RISE") {
+          btc_price.classList.add("rise");
+          btc_color.classList.add("rise");
+          btc_color.innerHTML =
+            arrow_up + (element.change_rate * 100).toFixed(2) + "%";
+        } else if (element.change === "FALL") {
+          btc_price.classList.add("fall");
+          btc_color.classList.add("fall");
+          btc_color.innerHTML =
+            arrow_down + (element.change_rate * 100).toFixed(2) + "%";
+        }
+        btc_price.innerText = element.trade_price
+          .toString()
+          .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+        break;
+      case "KRW-ETH": // 이더시세
+        console.log("KRW-ETH here");
+        if (element.change === "RISE") {
+          eth_price.classList.add("rise");
+          eth_color.classList.add("rise");
+          eth_color.innerHTML =
+            arrow_up +
+            element.change_price
+              .toString()
+              .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+        } else if (element.change === "FALL") {
+          eth_price.classList.add("fall");
+          eth_color.classList.add("fall");
+          eth_color.innerHTML =
+            arrow_down +
+            element.change_price
+              .toString()
+              .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+        }
+        eth_price.innerText = element.trade_price
+          .toString()
+          .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+    }
   });
 }
