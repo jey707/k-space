32def585f2b61ab04368bd44d7a6c5d804e78310
jey707 <jey707@naver.com>
2022-08-16 23:10:44 +0900

    header update

--- js/header.js
+++ js/header.js
@@ -1,1 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {});
+const body = document.querySelector("body");
+const searchWrap = document.querySelector(".search-box");
+const langBox = document.querySelector(".lang-box");
+
+function searchOnOff() {
+  if (searchWrap.classList.contains("box-off")) {
+    searchWrap.classList.replace("box-off", "box-on");
+    body.style.overflow = "hidden";
+  } else {
+    searchWrap.classList.replace("box-on", "box-off");
+    body.style.removeProperty("overflow");
+  }
+}
+
+function langOnOff() {
+  if (langBox.classList.contains("lang-hide")) {
+    langBox.classList.replace("lang-hide", "lang-show");
+  } else {
+    langBox.classList.replace("lang-show", "lang-hide");
+  }
+}
